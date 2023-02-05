2a2eaa17f9747b8d45733b0b22e4c3f6a7ac6239
Gabriel Chung <gabrielchung1128@gmail.com>
2023-02-05 21:27:09 +1100

    Improved version of keyword search

--- text_search_proverbs.py
+++ text_search_proverbs.py
@@ -5,10 +5,24 @@ proverbs_book_id = 19
 with open(f'./books/{proverbs_book_id}.json', 'r') as f:
     proverbs = json.load(f)
 
+def string_remove_all(input, remove_chars_list):
+    tmp = input
+    for remove_char in remove_chars_list:
+        tmp = tmp.replace(remove_char, '')
+    return tmp
+
+def tokenize_verse(verse):
+    punctuations_to_be_removed = ['.', ':', ',', ';']
+    tmp_verse = verse
+    tmp_verse = string_remove_all(tmp_verse, punctuations_to_be_removed)
+    tokens = tmp_verse.split(' ')
+    return tokens
+
 def text_search(keyword):
     for c_id, chapter in enumerate(proverbs):
         for v_id, verse in enumerate(chapter):
-            if keyword in verse:
+            tokens = tokenize_verse(verse)
+            if keyword in tokens:
                 print(f'{c_id+1:02}:{v_id+1:02} {verse}')
 
 
@@ -24,3 +38,4 @@ while(True):
     print()
     print('Results:')
     text_search(keyword)
+    print()
